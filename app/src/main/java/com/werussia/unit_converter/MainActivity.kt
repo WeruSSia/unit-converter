a95019f88be7688a7ffe7295b7840894e8579acb
Weronika Mirek <werussia16@gmail.com>
2020-08-20 00:29:46 +0200

    basic units added to spinners

--- app/src/main/java/com/werussia/unit_converter/MainActivity.kt
+++ app/src/main/java/com/werussia/unit_converter/MainActivity.kt
@@ -2,11 +2,29 @@ package com.werussia.unit_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSpinners()
+    }
+
+    fun setSpinners() {
+        val fromSpinner: Spinner = findViewById(R.id.from_unit_spinner)
+        val toSpinner: Spinner = findViewById(R.id.to_unit_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.units_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            fromSpinner.adapter = adapter
+            toSpinner.adapter = adapter
+        }
     }
 }
